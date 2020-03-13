3d0ef69b40af18c7663496388943bb743cf533fa
dengbiao <624796905@qq.com>
2020-03-13 18:05:24 +0800

    login

--- login/src/main/java/com/db/doudizhu/login/controller/UserController.java
+++ login/src/main/java/com/db/doudizhu/login/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.db.doudizhu.login.controller;
 
 import com.db.doudizhu.login.entity.User;
+import com.db.doudizhu.login.kit.ZKit;
 import com.db.doudizhu.login.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * date: 2020/2/26 14:54
+ * <p>
  * author: DengBiao
  */
 @RestController
@@ -14,18 +17,36 @@ public class UserController {
 
     private UserService userService;
 
+    @Autowired
+    private ZKit zKit;
+
     // 登录
     @RequestMapping("/login")
     public String login() {
-        String name = null, password = null;
+        String name = "null", password = "null";
         User user = new User(name, password);
         User u = userService.getUserByNameAndPw(user);
-        return "登录";
+        if (u != null) {
+            int result = userService.updateUserByUid(u);
+            if (result == 0) {
+                return "";
+            }
+        } else {
+            int result = userService.insetUser(user);
+            if (result == 0) {
+                return "";
+            }
+        }
+        String ip = zKit.selectServer();
+        return ip;
     }
 
     // 验证登录
     @RequestMapping("/verifyLogin")
     public String verifyLogin() {
+        String token = "null";
+        User user = new User(token);
+        User u = userService.getUserByToken(user);
         return "验证登录";
     }
 }
